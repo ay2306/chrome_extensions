6e8cfd0ecbd083e77588f50697adbca67a52b12e
ay2306 <mahajan.ayush2306@gmail.com>
2019-05-24 13:58:34 +0530

    First Javascript code for sample

---
+++ sample/bookmarklet.js
@@ -0,0 +1,8 @@
+
+(function(){
+    console.log("bookmarklet starting");
+    let paragraph = document.getElementsByTagName('p');
+    for(let i = 0; i < paragraph.length; ++i){
+        paragraph[i].innerHTML = 'ayush';
+    }
+})();
